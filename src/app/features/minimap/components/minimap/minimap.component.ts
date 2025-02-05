a92e7c7c5af5997b0797840535cfb2e6fbaffbfd
arielalvaro57666 <ariel57666@gmail.com>
2025-02-05 01:54:43 -0300

    f07 - repository stars fetch

--- src/app/features/minimap/components/minimap/minimap.component.ts
+++ src/app/features/minimap/components/minimap/minimap.component.ts
@@ -10,4 +10,20 @@ import { DescriptionDirective } from 'src/app/shared/directives/description/desc
 })
 export class MinimapComponent {
 
+    repo_url: string = `https://api.github.com/repos/arielalvaro57666/arielAlvaroPortfolio`
+    repo_stars: number = 0
+
+    getRepoStars() { 
+
+        fetch(this.repo_url)
+            .then(response => response.json())
+            .then(data => {this.repo_stars= data.stargazers_count})
+            .catch(error => console.error("Error obtaining repository data:", error));
+        }
+
+
+    ngOnInit() {
+        this.getRepoStars()
+    }
+
 }

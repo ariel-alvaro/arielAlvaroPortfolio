f85f3f82938057024a414c2d9dd81a27a1f1bab3
AlvaroAriel <ariel57666@gmail.com>
2025-02-24 16:05:54 -0300

    email send fetch test

--- src/app/features/contact/components/contact-menu/contact-menu.component.ts
+++ src/app/features/contact/components/contact-menu/contact-menu.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { EmailService } from '@features/contact/services/email.service';
 
 @Component({
   selector: 'app-contact-menu',
@@ -13,6 +14,11 @@ export class ContactMenuComponent {
     area_focused: boolean = false
     message: string = ""
 
+    constructor(public email_service: EmailService) {}
+
+    test() {
+      this.email_service.sendEmail(".")
+    }
 
     setAreaFocus() {
         console.log(this.message)

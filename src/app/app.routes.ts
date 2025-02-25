ed5214b92c9e736111243fd0273bba4460d80b31
AlvaroAriel <ariel57666@gmail.com>
2025-02-25 18:19:34 -0300

    feat: Lazy load all routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,20 @@ import { ResumeComponent } from '@features/principalmenu/components/resume/resum
 import { SkillsComponent } from '@features/principalmenu/components/skills/skills.component';
 
 export const routes: Routes = [
-    {path: "home", component: HomeComponent},
-    {path: "skills", component: SkillsComponent},
-    {path: "projects", component: ProjectsComponent},
-    {path: "education", component: EducationComponent},
-    {path: "resume", component: ResumeComponent},
+    {path: "home", loadComponent: () => import("./features/principalmenu/components/home/home.component").then(m => m.HomeComponent)},
+    {path: "skills", loadComponent: () => import("./features/principalmenu/components/skills/skills.component").then(m => m.SkillsComponent)},
+    {path: "projects", loadComponent: () => import("./features/principalmenu/components/projects/projects.component").then(m => m.ProjectsComponent)},
+    {path: "education", loadComponent: () => import("./features/principalmenu/components/education/education.component").then(m => m.EducationComponent)},
+    {path: "resume", loadComponent: () => import("./features/principalmenu/components/resume/resume.component").then(m => m.ResumeComponent)},
     {path: "", redirectTo: '/home', pathMatch: "full"},
 ];
+
+
+// export const routes: Routes = [
+//     {path: "home", component: HomeComponent},
+//     {path: "skills", component: SkillsComponent},
+//     {path: "projects", component: ProjectsComponent},
+//     {path: "education", component: EducationComponent},
+//     {path: "resume", component: ResumeComponent},
+//     {path: "", redirectTo: '/home', pathMatch: "full"},
+// ];
